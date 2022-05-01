3a8a6ccae371326d166293c6af30078178c20c80
Martin Sosa <martinsosa@outlook.com>
2022-05-01 18:19:17 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+
 import logo from './logo.svg';
+import NavBar from './components/NavBar/NavBar';
+
+
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <NavBar />
+
+
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +27,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
