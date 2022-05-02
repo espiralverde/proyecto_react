e2ae20150a52b4d6801eb0c0472ffaff331bd4c3
Martin Sosa <martinsosa@outlook.com>
2022-05-02 12:59:32 -0300

    2da Entrega

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,32 +1,34 @@
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+
+// import Container from 'react-bootstrap/Container'
+// import Nav from 'react-bootstrap/Nav'
+// import Navbar from 'react-bootstrap/Navbar'
+// import NavDropdown from 'react-bootstrap/NavDropdown'
 
 const NavBar = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">SteelBit</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <Nav.Link href="#features">Catálogo</Nav.Link>
+                        <Nav.Link href="#pricing">Insumos</Nav.Link>
+                        <NavDropdown title="Herramientas" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Batería</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Eléctricas</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Neumáticas</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">Manuales</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
-                    <Nav>
+                    {/* <Nav>
                         <Nav.Link href="#deets">More deets</Nav.Link>
                         <Nav.Link eventKey={2} href="#memes">
                             Dank memes
                         </Nav.Link>
-                    </Nav>
+                    </Nav> */}
                 </Navbar.Collapse>
             </Container>
         </Navbar>
